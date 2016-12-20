8d8e561cf30e3e06d7ae95c5ff985bb88eb3a4ce
Alex Yakhnin <Alex Yakhnin>
2016-12-20 11:33:33 -0500

    final clean up

--- motiondetector.js
+++ motiondetector.js
@@ -20,57 +20,7 @@ Module.register('motiondetector',{
 		if (notification === "USER_PRESENCE"){
 			this.sendNotification(notification, payload)
 		}
-	},
-	
-
-	notificationReceived: function (notification, payload, sender) {
-
-		var _this = self;
-
-		// if (notification === "DOM_OBJECTS_CREATED") {
-		// 	var video = document.createElement('video');
-		// 	var cameraPreview = document.createElement("div");
-		// 	cameraPreview.id = "cameraPreview";
-		// 	cameraPreview.style = "visibility:hidden;"
-		// 	cameraPreview.appendChild(video);
-
-		// 	var canvas = document.createElement('canvas');
-
-		// 	DiffCamEngine.init({
-		// 		video: video,
-		// 		motionCanvas: canvas,
-		// 		initSuccessCallback: function () {
-		// 			DiffCamEngine.start();
-		// 		},
-		// 		initErrorCallback: function () {
-		// 			console.log('error init cam engine');
-		// 		},
-		// 		captureCallback: _this.onCapture
-		// 	});
-		// }
-	},
-
-	onCapture: function (payload) {
-		var _this = this;
-		var score = payload.score;
-		if (score > 20) {
-			_this.lastTimeMotionDetected = new Date();
-			if (_this.poweredOff) {
-				_this.poweredOff = false;
-				_this.sendSocketNotification('MOTION_DETECTED', _this.config);
-			}
-		}
-		else {
-			var currentDate = new Date();
-			var time = currentDate.getTime() - _this.lastTimeMotionDetected;
-			if ((time > _this.config.timeout) && (!_this.poweredOff)) {
-				_this.sendSocketNotification('DEACTIVATE_MONITOR', _this.config);
-				_this.sendNotification('DEACTIVATE_MONITOR', _this.config);
-				_this.poweredOff = true;
-			}
-		}
-		console.log('score:' + score);
-	},
+	},		
 
 	start: function() {
 		Log.info('Starting module: ' + this.name);
