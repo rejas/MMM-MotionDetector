752d0a9675d7ab1f8b861231ed0aa826fa812284
Veeck <github@veeck.de>
2018-07-18 20:25:43 +0200

    Log errors

--- diff-cam-engine.js
+++ diff-cam-engine.js
@@ -112,7 +112,7 @@ var DiffCamEngine = (function() {
                 }
             })
             .catch( function(err) {
-                Log.error(err);
+                initError(err);
             });
 
         /*
