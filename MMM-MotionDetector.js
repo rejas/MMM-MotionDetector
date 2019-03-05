d071040711eff67b76f8570892a60dd4f8e1c0a9
rejas <github@veeck.de>
2019-03-05 23:26:49 +0100

    Use eslint config from MagicMirror project

--- MMM-MotionDetector.js
+++ MMM-MotionDetector.js
@@ -1,94 +1,94 @@
 /* global DiffCamEngine, Log, Module */
-'use strict';
+"use strict";
 
-Module.register('MMM-MotionDetector',{
+Module.register("MMM-MotionDetector",{
 
-    defaults: {
-        captureIntervalTime: 1000, // 1 second
-        scoreThreshold: 20,
-        timeout: 120000 // 2 minutes
-    },
+	defaults: {
+		captureIntervalTime: 1000, // 1 second
+		scoreThreshold: 20,
+		timeout: 120000 // 2 minutes
+	},
 
-    lastScoreDetected: null,
-    lastTimeMotionDetected: null,
-    poweredOff: false,
+	lastScoreDetected: null,
+	lastTimeMotionDetected: null,
+	poweredOff: false,
 
-    getDom: function () {
-        let wrapper = document.createElement("div");
-        let headline = document.createElement("h3");
-        headline.innerHTML = "MMM-Motion";
-        wrapper.appendChild(headline);
-        let score = document.createElement("p");
-        score.innerHTML = "last score: " + this.lastScoreDetected;
-        wrapper.appendChild(score);
-        let time = document.createElement("p");
-        time.innerHTML = "last time motion detected: " + this.lastTimeMotionDetected.toLocaleTimeString();
-        wrapper.appendChild(time);
-        return wrapper;
-    },
+	getDom: function () {
+		let wrapper = document.createElement("div");
+		let headline = document.createElement("h3");
+		headline.innerHTML = "MMM-Motion";
+		wrapper.appendChild(headline);
+		let score = document.createElement("p");
+		score.innerHTML = "last score: " + this.lastScoreDetected;
+		wrapper.appendChild(score);
+		let time = document.createElement("p");
+		time.innerHTML = "last time motion detected: " + this.lastTimeMotionDetected.toLocaleTimeString();
+		wrapper.appendChild(time);
+		return wrapper;
+	},
 
-    getScripts: function() {
-        return ['diff-cam-engine.js'];
-    },
+	getScripts: function() {
+		return ["diff-cam-engine.js"];
+	},
 
-    // Override socket notification handler.
-    socketNotificationReceived: function(notification, payload) {
-        if (notification === 'USER_PRESENCE') {
-            this.sendNotification(notification, payload)
-        }
-    },
+	// Override socket notification handler.
+	socketNotificationReceived: function(notification, payload) {
+		if (notification === "USER_PRESENCE") {
+			this.sendNotification(notification, payload)
+		}
+	},
 
-    start: function() {
-        Log.info('MMM-MotionDetector: starting up');
+	start: function() {
+		Log.info("MMM-MotionDetector: starting up");
 
-        this.lastScoreDetected = 0;
-        this.lastTimeMotionDetected = new Date();
+		this.lastScoreDetected = 0;
+		this.lastTimeMotionDetected = new Date();
 
-        // make sure that the monitor is on when starting
-        this.sendSocketNotification('MOTION_DETECTED', 0);
+		// make sure that the monitor is on when starting
+		this.sendSocketNotification("MOTION_DETECTED", 0);
 
-        let _this = this;
-        let canvas = document.createElement('canvas');
-        let video = document.createElement('video');
-        let cameraPreview = document.createElement('div');
-        cameraPreview.id = 'cameraPreview';
-        cameraPreview.style = 'visibility:hidden;';
-        cameraPreview.appendChild(video);
+		let _this = this;
+		let canvas = document.createElement("canvas");
+		let video = document.createElement("video");
+		let cameraPreview = document.createElement("div");
+		cameraPreview.id = "cameraPreview";
+		cameraPreview.style = "visibility:hidden;";
+		cameraPreview.appendChild(video);
 
-        DiffCamEngine.init({
-            video: video,
-            captureIntervalTime: _this.config.captureIntervalTime,
-            motionCanvas: canvas,
-            initSuccessCallback: function () {
-                Log.info('MMM-MotionDetector: DiffCamEngine init successful');
-                DiffCamEngine.start();
-            },
-            initErrorCallback: function (error) {
-                Log.error('MMM-MotionDetector: DiffCamEngine init failed, error:');
-                Log.error(error);
-            },
-            captureCallback: function(payload) {
-                const score = payload.score;
-                if (score > _this.config.scoreThreshold) {
-                    _this.lastTimeMotionDetected = new Date();
-                    if (_this.poweredOff) {
-                        _this.sendSocketNotification('MOTION_DETECTED', score);
-                        _this.poweredOff = false;
-                    }
-                }
-                else {
-                    const currentDate = new Date(),
-                        time = currentDate.getTime() - _this.lastTimeMotionDetected;
-                    if ((time > _this.config.timeout) && (!_this.poweredOff)) {
-                        _this.sendSocketNotification('DEACTIVATE_MONITOR', _this.config);
-                        _this.sendNotification('DEACTIVATE_MONITOR', _this.config);
-                        _this.poweredOff = true;
-                    }
-                }
-                _this.lastScoreDetected = score;
-                _this.updateDom();
-                Log.info('MMM-MotionDetector: score ' + score);
-            }
-        });
-    }
+		DiffCamEngine.init({
+			video: video,
+			captureIntervalTime: _this.config.captureIntervalTime,
+			motionCanvas: canvas,
+			initSuccessCallback: function () {
+				Log.info("MMM-MotionDetector: DiffCamEngine init successful");
+				DiffCamEngine.start();
+			},
+			initErrorCallback: function (error) {
+				Log.error("MMM-MotionDetector: DiffCamEngine init failed, error:");
+				Log.error(error);
+			},
+			captureCallback: function(payload) {
+				const score = payload.score;
+				if (score > _this.config.scoreThreshold) {
+					_this.lastTimeMotionDetected = new Date();
+					if (_this.poweredOff) {
+						_this.sendSocketNotification("MOTION_DETECTED", score);
+						_this.poweredOff = false;
+					}
+				}
+				else {
+					const currentDate = new Date(),
+						time = currentDate.getTime() - _this.lastTimeMotionDetected;
+					if ((time > _this.config.timeout) && (!_this.poweredOff)) {
+						_this.sendSocketNotification("DEACTIVATE_MONITOR", _this.config);
+						_this.sendNotification("DEACTIVATE_MONITOR", _this.config);
+						_this.poweredOff = true;
+					}
+				}
+				_this.lastScoreDetected = score;
+				_this.updateDom();
+				Log.info("MMM-MotionDetector: score " + score);
+			}
+		});
+	}
 });
--- node_helper.js
+++ node_helper.js
@@ -1,68 +1,68 @@
-'use strict';
-const NodeHelper = require('node_helper');
-const exec = require('child_process').exec;
+"use strict";
+const NodeHelper = require("node_helper");
+const exec = require("child_process").exec;
 
 module.exports = NodeHelper.create({
-    start: function () {
-        this.started = false;
-        this.isMonitorOn(function(result) {
-            console.log('MMM-MotionDetector: monitor on ' + result);
-        });
-    },
+	start: function () {
+		this.started = false;
+		this.isMonitorOn(function(result) {
+			console.log("MMM-MotionDetector: monitor on " + result);
+		});
+	},
 
-    activateMonitor: function () {
-        this.isMonitorOn(function(result) {
-            if (!result) {
-                exec('vcgencmd display_power 1', function(err, out, code) {
-                    if (err) {
-                        console.error('MMM-MotionDetector: error activating monitor: ' + code);
-                    } else {
-                        console.log('MMM-MotionDetector: monitor has been activated');
-                    }
-                });
-            }
-        });
-        this.started = false;
-    },
+	activateMonitor: function () {
+		this.isMonitorOn(function(result) {
+			if (!result) {
+				exec("vcgencmd display_power 1", function(err, out, code) {
+					if (err) {
+						console.error("MMM-MotionDetector: error activating monitor: " + code);
+					} else {
+						console.log("MMM-MotionDetector: monitor has been activated");
+					}
+				});
+			}
+		});
+		this.started = false;
+	},
 
-    deactivateMonitor: function () {
-        this.isMonitorOn(function(result) {
-            if (result) {
-                exec('vcgencmd display_power 0', function(err, out, code) {
-                    if (err) {
-                        console.error('MMM-MotionDetector: error deactivating monitor: ' + code);
-                    } else {
-                        console.log('MMM-MotionDetector: monitor has been deactivated');
-                    }
-                });
-            }
-        });
-        this.started = false;
-    },
+	deactivateMonitor: function () {
+		this.isMonitorOn(function(result) {
+			if (result) {
+				exec("vcgencmd display_power 0", function(err, out, code) {
+					if (err) {
+						console.error("MMM-MotionDetector: error deactivating monitor: " + code);
+					} else {
+						console.log("MMM-MotionDetector: monitor has been deactivated");
+					}
+				});
+			}
+		});
+		this.started = false;
+	},
 
-    isMonitorOn: function(resultCallback) {
-        exec('vcgencmd display_power', function(err, out, code) {
-            if (err) {
-                console.error('MMM-MotionDetector: error calling monitor status: ' + code);
-                return;
-            }
+	isMonitorOn: function(resultCallback) {
+		exec("vcgencmd display_power", function(err, out, code) {
+			if (err) {
+				console.error("MMM-MotionDetector: error calling monitor status: " + code);
+				return;
+			}
 
-            console.log('MMM-MotionDetector: monitor ' + out);
-            resultCallback(out.includes('=1'));
-        });
-    },
+			console.log("MMM-MotionDetector: monitor " + out);
+			resultCallback(out.includes("=1"));
+		});
+	},
 
-    // Subclass socketNotificationReceived received.
-    socketNotificationReceived: function (notification, payload) {
-        if (notification === 'MOTION_DETECTED' && this.started === false) {
-            console.log('MMM-MotionDetector: MOTION_DETECTED, score ' + payload);
-            this.started = true;
-            this.activateMonitor();
-        }
-        if (notification === 'DEACTIVATE_MONITOR' && this.started === false) {
-            console.log('MMM-MotionDetector: DEACTIVATE_MONITOR');
-            this.started = true;
-            this.deactivateMonitor();
-        }
-    }
+	// Subclass socketNotificationReceived received.
+	socketNotificationReceived: function (notification, payload) {
+		if (notification === "MOTION_DETECTED" && this.started === false) {
+			console.log("MMM-MotionDetector: MOTION_DETECTED, score " + payload);
+			this.started = true;
+			this.activateMonitor();
+		}
+		if (notification === "DEACTIVATE_MONITOR" && this.started === false) {
+			console.log("MMM-MotionDetector: DEACTIVATE_MONITOR");
+			this.started = true;
+			this.deactivateMonitor();
+		}
+	}
 });
