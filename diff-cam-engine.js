386e6ea6c509a4a380565f0cebe0cf461600dffa
rejas <github@veeck.de>
2019-03-08 12:05:55 +0100

    Update getUserMedia code for modern browsers (#14)
    * Add working getUserMedia code
    
    * Use old event just to be sure :-)

--- diff-cam-engine.js
+++ diff-cam-engine.js
@@ -75,28 +75,49 @@ const DiffCamEngine = (function() {
 		motionCanvas.height = diffHeight;
 		motionContext = motionCanvas.getContext("2d");
 
-		// requestWebcam
-		(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia).call(
-			navigator,
-			{video: true},
-			function(localMediaStream) {
-				if(video) {
-					const vendorURL = window.URL || window.webkitURL;
+		// Older browsers might not implement mediaDevices at all, so we set an empty object first
+		if (navigator.mediaDevices === undefined) {
+			navigator.mediaDevices = {};
+		}
 
-					if (navigator.mozGetUserMedia) {
-						video.mozSrcObject = localMediaStream;
-						video.play();
-					} else {
-						video.src = vendorURL.createObjectURL(localMediaStream);
-					}
-					stream = localMediaStream;
-					initSuccessCallback();
+		// Some browsers partially implement mediaDevices. We can't just assign an object
+		// with getUserMedia as it would overwrite existing properties.
+		// Here, we will just add the getUserMedia property if it's missing.
+		if (navigator.mediaDevices.getUserMedia === undefined) {
+			navigator.mediaDevices.getUserMedia = function(constraints) {
+
+				// First get a hold of the legacy getUserMedia, if present
+				var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+				// Some browsers just don't implement it - return a rejected promise with an error
+				// to keep a consistent interface
+				if (!getUserMedia) {
+					return Promise.reject(new Error("getUserMedia is not implemented in this browser"));
 				}
-			},
-			function(error) {
-				initErrorCallback(error);
+
+				// Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+				return new Promise(function(resolve, reject) {
+					getUserMedia.call(navigator, constraints, resolve, reject);
+				});
 			}
-		);
+		}
+
+		// request webcam
+		navigator.mediaDevices.getUserMedia({ video: true })
+			.then(function(localMediaStream) {
+				// Older browsers may not have srcObject
+				if ("srcObject" in video) {
+					video.srcObject = localMediaStream;
+				} else {
+					// Avoid using this in new browsers, as it is going away.
+					video.src = window.URL.createObjectURL(localMediaStream);
+				}
+				stream = localMediaStream;
+				initSuccessCallback();
+			})
+			.catch(function(err) {
+				initErrorCallback(err);
+			});
 	}
 
 	function start() {
@@ -106,7 +127,6 @@ const DiffCamEngine = (function() {
 
 		// streaming takes a moment to start
 		video.addEventListener("canplay", startComplete);
-		video.srcObject = stream;
 	}
 
 	function startComplete() {
